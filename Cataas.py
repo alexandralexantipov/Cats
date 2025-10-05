46ff5fbc93cf0f5712468b81895dfa0c97602f32
Alexandr Antipov <alexandralexantipov@gmail.com>
2025-10-05 07:30:36 +0300

    Добавлено получение изображений по тегу, а также без тега

--- Cataas.py
+++ Cataas.py
@@ -18,16 +18,17 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
 
     if img:
         new_window = Toplevel()
         new_window.title("Картинка с котиком")
-        new_window.geometry(600x480)
+        new_window.geometry("600x480")
         label = Label(new_window, image=img)
         label.pack()
         label.image = img
-
 
 
 def exit():
@@ -38,10 +39,12 @@ window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
+tag_entry = Entry()
+tag_entry.pack()
 
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
-# update_button = Button(text="Обновить", command=set_image)
-# update_button.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -52,8 +55,6 @@ file_menu.add_command(label="Загрузить фото", command=open_new_wind
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
-
-set_image()
 
 window.mainloop()
 
